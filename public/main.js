37984cba678a3fc5dfff18a38ea10d22f2d35d69
MaHo <marvin@raithweg15.de>
2021-04-15 18:32:34 +0200

    ++

--- public/main.js
+++ public/main.js
@@ -10,13 +10,13 @@ ws.onmessage = function (event) {
     {
         case "temp" :
             playlistTitles= JSON.parse(event.data[1]);
-            const temperatur = event.data[1];
+            let temperatur = event.data[1];
             console.log("Temperatur:");
             console.log(temperatur);
         break;
         case "feucht" :
             playlist = JSON.parse(event.data[1]); //array 2 
-            const feuchtigkeit = event.data[2];
+            let feuchtigkeit = event.data[2];
             console.log("Feuchtigkeit:");
             console.log(feuchtigkeit);
         break;
