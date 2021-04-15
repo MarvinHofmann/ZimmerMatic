61bdc8f3b000cd3d34416d2422133d09dcef6118
MaHo <marvin@raithweg15.de>
2021-04-15 18:30:45 +0200

    ++

--- public/main.js
+++ public/main.js
@@ -1,20 +1,23 @@
-const ws = new WebSocket('ws://raspberrypi:8080');
+const ws = new WebSocket('ws://ZimmerMatic:8080');
+
 ws.addEventListener("open", () => {
     console.log("Client connected with server!")
 
 })
 ws.onmessage = function (event) {
-
+    console.log("Daten kamen an");
     switch (event.data[0])
     {
         case "temp" :
             playlistTitles= JSON.parse(event.data[1]);
             const temperatur = event.data[1];
+            console.log("Temperatur:");
             console.log(temperatur);
         break;
         case "feucht" :
             playlist = JSON.parse(event.data[1]); //array 2 
             const feuchtigkeit = event.data[2];
+            console.log("Feuchtigkeit:");
             console.log(feuchtigkeit);
         break;
     }
