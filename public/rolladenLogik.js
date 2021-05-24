c8c3c8bbf01e6eaec7df6d50ce3082ca49169f83
MaHo <marvin@raithweg15.de>
2021-05-24 22:50:42 +0200

    ++

--- public/rolladenLogik.js
+++ public/rolladenLogik.js
@@ -37,6 +37,6 @@ function send(){
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({time: document.getElementById("inputTime").value()})
+    body: JSON.stringify({time: document.getElementById("inputTime").value})
   });
 }
