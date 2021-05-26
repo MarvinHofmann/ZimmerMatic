f3f45f1ee40550d288703ec4c4e0da37873b8c84
MaHo <marvin@raithweg15.de>
2021-05-26 23:14:41 +0200

    Routinen Ausgabe

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 //Websocket Server
 const WebSocket = require("ws");
 const wss = new WebSocket.Server({ port: 8000 }); // abgespilteter WS Server auf anderem Port
-const schedule = require('node-schedule');
+const schedule = require("node-schedule");
 let currentClientsws = [];
 
 // Init. EXpress Server
@@ -14,7 +14,7 @@ let bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
 //express.static sucht im Ordner public nach der Index.js Datei und publisht sie direkt
-app.use(express.static("public")); 
+app.use(express.static("public"));
 
 app.listen(port, () => {
   console.log(`App listening at http://ZimmerMatic:${port}`); // Publisher Server auf Port 3443
@@ -22,9 +22,21 @@ app.listen(port, () => {
 });
 
 //Globale Variablen
-let temp, feucht, temp2, feucht2, temp3, feucht3, zeit1, zeit2, zeit3, anzClients = 1, plFeucht1, plFeucht2, plFeucht3;
+let temp,
+  feucht,
+  temp2,
+  feucht2,
+  temp3,
+  feucht3,
+  zeit1,
+  zeit2,
+  zeit3,
+  anzClients = 1,
+  plFeucht1,
+  plFeucht2,
+  plFeucht3;
 //D1 Mini Whitelist, um ihm besondere Dinge zu senden
-let d1 = "::ffff:192.168.0.62"; 
+let d1 = "::ffff:192.168.0.62";
 
 //1.HTTP Get request
 app.get("/", function (request, response) {
@@ -49,7 +61,9 @@ app.post("/senderZwei", function (req, res) {
   temp2 = req.body.temperatur;
   feucht2 = req.body.feuchtigkeit;
   zeit2 = berechneZeit();
-  console.log("Temperatur2: " + temp2 + " Feuchtigkeit2: " + feucht2 + " Zeit: " + zeit2);
+  console.log(
+    "Temperatur2: " + temp2 + " Feuchtigkeit2: " + feucht2 + " Zeit: " + zeit2
+  );
   broadcast(feucht2, temp2, zeit2, "S2");
   res.sendStatus(200);
 });
@@ -58,7 +72,9 @@ app.post("/senderDrei", function (req, res) {
   temp3 = req.body.temperatur;
   feucht3 = req.body.feuchtigkeit;
   zeit3 = berechneZeit();
-  console.log("Temperatur3: " + temp3 + " Feuchtigkeit3: " + feucht3 + " Zeit: " + zeit3);
+  console.log(
+    "Temperatur3: " + temp3 + " Feuchtigkeit3: " + feucht3 + " Zeit: " + zeit3
+  );
   broadcast(feucht3, temp3, zeit3, "S3");
   res.sendStatus(200);
 });
@@ -80,7 +96,7 @@ app.post("/plfanze3", function (req, res) {
 //Sagt euch wenn ein Client verbunden ist oder wenn er disconnected
 wss.on("connection", function connection(ws, req) {
   console.log("Client connected!");
-  
+
   const ip = req.socket.remoteAddress;
   console.log(ip);
   if (ip === d1) {
@@ -92,29 +108,30 @@ wss.on("connection", function connection(ws, req) {
     broadcast(feucht, temp, zeit1, "S1");
     broadcast(feucht2, temp2, zeit2, "S2");
     broadcast(feucht3, temp3, zeit3, "S3");
+    broadcastRoutinen();
   }
   //Sendet dem D1 mini als besonderen Client die Anweisungen hoch runter stop
   ws.on("message", function incoming(message) {
     console.log("received: %s", message);
-    if(currentClientsws[0] != null){
+    if (currentClientsws[0] != null) {
       switch (message) {
-        case 'hoch':
+        case "hoch":
           currentClientsws[0].send("99");
           rolStatus = 0;
           break;
-        case 'stop':
+        case "stop":
           rolStatus = 1;
           currentClientsws[0].send("100");
           break;
-        case 'runter':
+        case "runter":
           rolStatus = 2;
           currentClientsws[0].send("101");
           break;
       }
-    }  
+    }
   });
- 
-ws.on("close", (data) => {
+
+  ws.on("close", (data) => {
     console.log("Client has disconnceted");
   });
 });
@@ -122,9 +139,15 @@ ws.on("close", (data) => {
 // diese funktion schickt das übergebene Objekt , int , string oder json an alle verbundenen Clients
 function broadcast(feucht, temp, zeit, sender) {
   for (let i = 1; i < currentClientsws.length; i++) {
-    currentClientsws[i].send(JSON.stringify({ type: "feuchtigkeit" + sender, value: feucht }));
-    currentClientsws[i].send(JSON.stringify({ type: "temperatur" + sender, value: temp }));
-    currentClientsws[i].send(JSON.stringify({ type: "zeit" + sender, value: zeit }));
+    currentClientsws[i].send(
+      JSON.stringify({ type: "feuchtigkeit" + sender, value: feucht })
+    );
+    currentClientsws[i].send(
+      JSON.stringify({ type: "temperatur" + sender, value: temp })
+    );
+    currentClientsws[i].send(
+      JSON.stringify({ type: "zeit" + sender, value: zeit })
+    );
   }
 }
 
@@ -151,21 +174,24 @@ function berechneZeit() {
 
 let timeArray = [];
 let jobArray = [];
+let richArray = [];
 aCoutn = 0;
 
-function erstelleRoutine(richtung){
-  jobArray[aCoutn] = schedule.scheduleJob(timeArray[aCoutn], function(){
+function erstelleRoutine(richtung) {
+  jobArray[aCoutn] = schedule.scheduleJob(timeArray[aCoutn], function () {
     console.log("Führe Routine aus");
     currentClientsws[0].send(richtung);
   });
 }
 
-function loescheRoutine(index){
+function loescheRoutine(index) {
   jobArray[index].cancel();
-  jobArray.splice(index,1);
-  timeArray.splice(index,1);
+  jobArray.splice(index, 1);
+  timeArray.splice(index, 1);
+  richArray.splice(index, 1);
   jobArray = cleanArray(jobArray);
-  timeArray= cleanArray(timeArray); 
+  timeArray = cleanArray(timeArray);
+  richArray = cleanArray(richArray);
   aCoutn--;
   console.log("routine gelöscht");
 }
@@ -173,10 +199,11 @@ function loescheRoutine(index){
 app.post("/create", function (request, response) {
   console.log("Eingehende post request");
   timeArray[aCoutn] = request.body.time;
+  richArray[aCoutn] = getRichtung(request.body.richtung);
   erstelleRoutine(request.body.richtung);
-  console.log(request.body.richtung);
-  console.log(jobArray)
+  console.log(jobArray);
   console.log(timeArray);
+  console.log(richArray);
   aCoutn++;
   response.sendStatus(200);
 });
@@ -184,7 +211,7 @@ app.post("/create", function (request, response) {
 app.post("/deleteR", function (request, response) {
   console.log("Eingehende delete request");
   loescheRoutine(request.body.num);
-  console.log(jobArray)
+  console.log(jobArray);
   console.log(timeArray);
   response.sendStatus(200);
 });
@@ -197,4 +224,26 @@ function cleanArray(actual) {
     }
   }
   return newArray;
+}
+
+function broadcastRoutinen() {
+  for (let i = 1; i < aCoutn; i++) {
+    currentClientsws[i].send(
+      JSON.stringify({ type: "routineT" + i, value: timeArray[i] })
+    );
+    currentClientsws[i].send(
+      JSON.stringify({ type: "routineR" + i, value: richArray[i] })
+    );
+  }
+}
+
+function getRichtung(num) {
+  switch (num) {
+    case 99:
+      return "Hoch";
+    case 101:
+      return "Runter"
+    default:
+      break;
+  }
 }
--- public/rolladenLogik.js
+++ public/rolladenLogik.js
@@ -3,11 +3,40 @@ let aktRoutine;
 ws.addEventListener("open", (message) => {
   console.log("Client connected with server!");
 });
+let counter = 0;
 ws.addEventListener("message", function (event) {
   const data = event.data;
-
-  if (data === "now") {
-    btnfunction(3);
+  switch (data.type) {
+    case "routineT1":
+      document.getElementById("Routine1").innerTex = data.value;
+      break;
+    case "routineT2":
+      document.getElementById("Routine2").innerTex = data.value;
+      break;
+    case "routineT3":
+      document.getElementById("Routine3").innerTex = data.value;
+      break;
+    case "routineT4":
+      document.getElementById("Routine4").innerTex = data.value;
+      break;
+    case "routineT5":
+      document.getElementById("Routine5").innerTex = data.value;
+      break;
+    case "routineR1":
+      document.getElementById("RoutineR1").innerTex = data.value;
+      break;
+    case "routineR2":
+      document.getElementById("RoutineR2").innerTex = data.value;
+      break;
+    case "routineR3":
+      document.getElementById("RoutineR3").innerTex = data.value;
+      break;
+    case "routineR4":
+      document.getElementById("RoutineR4").innerTex = data.value;
+      break;
+    case "routineR5":
+      document.getElementById("RoutineR5").innerTex = data.value;
+      break;
   }
 });
 
@@ -62,56 +91,3 @@ function sendDelete() {
   document.getElementById("inputIndexDelete").innerText =
     "Erfolgreich gelöscht";
 }
-
-$("input[type='number']").inputSpinner({
-
-  // button text/icons
-  decrementButton: "<strong>-</strong>", 
-  incrementButton: "<strong>+</strong>", 
-
-  // class of input group
-  groupClass: "input-group-spinner",
-
-  // button class
-  buttonsClass: "btn-outline-secondary",
-
-  // button width
-  buttonsWidth: "2.5em",
-
-  // text alignment
-  textAlign: "center",
-
-  // delay in milliseconds
-  autoDelay: 500, 
-
-  // interval in milliseconds
-  autoInterval: 100,
-
-  // set this `true` to disable the possibility to enter or paste the number via keyboard
-  buttonsOnly: false, 
-
-  // set this to `false` to disallow the use of the up and down arrow keys to step
-  keyboardStepping: true, 
-
-  // the locale, per default detected automatically from the browser
-  locale: navigator.language, 
-
-  // the editor (parsing and rendering of the input)
-  editor: I18nEditor, 
-  
-  // the template of the input
-  template: // the template of the input
-    '<div class="input-group ${groupClass}">' +
-    '<div class="input-group-prepend"><button style="min-width: ${buttonsWidth}" class="btn btn-decrement ${buttonsClass} btn-minus" type="button">${decrementButton}</button></div>' +
-    '<input type="text" inputmode="decimal" style="text-align: ${textAlign}" class="form-control form-control-text-input"/>' +
-    '<div class="input-group-append"><button style="min-width: ${buttonsWidth}" class="btn btn-increment ${buttonsClass} btn-plus" type="button">${incrementButton}</button></div>' +
-    '</div>'
-  
-});
-
-changedElement.addEventListener("change",function(event) {
-    valueOutput.innerHTML = changedElement.value
-    instance.setValue(new value)
-    instance.destroy()
-})
-  
