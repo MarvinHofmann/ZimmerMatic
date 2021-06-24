fd0bcdc75042312fe0f5a549f4af637ba8601010
MaHo <marvin@raithweg15.de>
2021-06-24 18:45:00 +0200

    ++

--- index.js
+++ index.js
@@ -34,6 +34,8 @@ app.listen(port, () => {
 });
 
 const TelegramBot = require('node-telegram-bot-api');
+const token = '1885391976:AAE4_R-CQXXFZxIuz_AxFIyF6BuTdaWV2HM';
+//const token1 = '1710906682:AAFpnBEr_fTgOGsQNN0zdjDcvzqIW_SBns4';
 const bot = new TelegramBot(token, {polling: true});
 
 //Globale Variablen
@@ -108,9 +110,6 @@ app.post("/senderZwei", function (req, res) {
   broadcast(feucht2, temp2, zeit2, "S2");
   res.sendStatus(200);
 });
-
-const token = '1885391976:AAE4_R-CQXXFZxIuz_AxFIyF6BuTdaWV2HM';
-//const token1 = '1710906682:AAFpnBEr_fTgOGsQNN0zdjDcvzqIW_SBns4';
 
 app.post("/senderDrei", function (req, res) {
   temp3 = req.body.temperatur;
@@ -404,9 +403,13 @@ let chatId;
 bot.on('message', (msg) => {
   chatId = msg.chat.id;
   console.log(chatId);
+  console.log(msg.text);
   switch (msg.text) {
-    case "temp":
+    case "/temp":
       bot.sendMessage(chatId, "Die Temperatur im Mittel beträgt: " + average)
+      break;
+    case "/temp@zimmerMatic_Bot":
+        bot.sendMessage(chatId, "Die Temperatur im Mittel beträgt: " + average)
       break;
       case "rs":
       currentClientsws[0].send("0");
