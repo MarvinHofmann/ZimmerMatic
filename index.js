56848654d5e05c40804d4660ddbc427ab831510f
MarvinHofmann <82021987+MarvinHofmann@users.noreply.github.com>
2021-07-15 21:01:10 +0200

    ++

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ exports.app = app;
 
 const port = 3443;
 let bodyParser = require("body-parser");
+//exports.bodyParser = bodyParser;
 app.use(bodyParser.json());
 const path = require("path");
 //express.static sucht im Ordner public nach der Index.js Datei und publisht sie direkt
@@ -50,9 +51,6 @@ let temp,
   zeit2,
   zeit3,
   anzClients = 2,
-  plFeucht1,
-  plFeucht2,
-  plFeucht3,
   average,
   plZeit1,
   plZeit2,
@@ -139,43 +137,7 @@ app.post("/senderDrei", function (req, res) {
   res.sendStatus(200);
 });
 
-/********************************Pflanzenüberwachung*********************************************/
-app.post("/plfanze1", function (req, res) {
-  plFeucht1 = req.body.feuchtigkeit;
-  console.log("Pflanze 1: " + plFeucht1);
-  plZeit1 = berechneZeit();
-  broadcastPflanzen(plFeucht1, plZeit1, "S1");
-  if (plFeucht1 >= 440) {
-    //390 - 440
-    bot.sendMessage(chatId, "Pflanze 1 bitte Gießen!");
-  }
-  res.sendStatus(200);
-});
 
-app.post("/plfanze2", function (req, res) {
-  plFeucht2 = req.body.feuchtigkeit;
-  console.log("Pflanze 2: " + plFeucht2);
-  plZeit2 = berechneZeit();
-  broadcastPflanzen(plFeucht2, plZeit2, "S2");
-  if (plFeucht2 >= 320) {
-    //380-410
-    bot.sendMessage(chatId, "Pflanze 2 bitte Gießen!");
-  }
-  res.sendStatus(200);
-});
-
-app.post("/plfanze3", function (req, res) {
-  plFeucht3 = req.body.feuchtigkeit;
-  console.log("Pflanze 3: " + plFeucht3);
-  plZeit3 = berechneZeit();
-  broadcastPflanzen(plFeucht3, plZeit3, "S3");
-  if (plFeucht3 >= 180) {
-    //zw 180 && 200
-    bot.sendMessage(chatId, "Pflanze 2 bitte Gießen!");
-  }
-  res.sendStatus(200);
-});
-/***********************************************************************************************/
 
 //Sagt euch wenn ein Client verbunden ist oder wenn er disconnected
 wss.on("connection", function connection(ws, req) {
--- modules/pflanzen.js
+++ modules/pflanzen.js
@@ -1,7 +1,41 @@
 const main = require("../index.js");
-
-main.app.get("/test", function (req, res) {
-   console.log(req);
-   console.log("funktioniert");
-   res.sendStatus(200);
-});
+let plFeucht1,
+plFeucht2,
+plFeucht3;
+/********************************Pflanzenüberwachung*********************************************/
+main.app.post("/plfanze1", function (req, res) {
+    plFeucht1 = req.body.feuchtigkeit;
+    console.log("Pflanze 1: " + plFeucht1);
+    plZeit1 = berechneZeit();
+    broadcastPflanzen(plFeucht1, plZeit1, "S1");
+    if (plFeucht1 >= 440) {
+      //390 - 440
+      bot.sendMessage(chatId, "Pflanze 1 bitte Gießen!");
+    }
+    res.sendStatus(200);
+  });
+  
+main.app.post("/plfanze2", function (req, res) {
+    plFeucht2 = req.body.feuchtigkeit;
+    console.log("Pflanze 2: " + plFeucht2);
+    plZeit2 = berechneZeit();
+    broadcastPflanzen(plFeucht2, plZeit2, "S2");
+    if (plFeucht2 >= 320) {
+      //380-410
+      bot.sendMessage(chatId, "Pflanze 2 bitte Gießen!");
+    }
+    res.sendStatus(200);
+  });
+  
+  main.app.post("/plfanze3", function (req, res) {
+    plFeucht3 = req.body.feuchtigkeit;
+    console.log("Pflanze 3: " + plFeucht3);
+    plZeit3 = berechneZeit();
+    broadcastPflanzen(plFeucht3, plZeit3, "S3");
+    if (plFeucht3 >= 180) {
+      //zw 180 && 200
+      bot.sendMessage(chatId, "Pflanze 2 bitte Gießen!");
+    }
+    res.sendStatus(200);
+  });
+  /***********************************************************************************************/
