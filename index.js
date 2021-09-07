5e20b8423eb73d9b536188a0872d5fddfdcf3867
MarvinHofmann <82021987+MarvinHofmann@users.noreply.github.com>
2021-09-07 16:33:47 +0200

    ++

--- index.js
+++ index.js
@@ -85,6 +85,13 @@ app.get('/DownloadLog', function(req, res){
   res.download(file); // Set disposition and send it.
 });
 
+app.get('/DownloadLogCom', function(req, res){
+  const file = `${__dirname}/log-file.txt`;
+  console.log("Anfrage kam an");
+  console.log(file);
+  res.download(file); // Set disposition and send it.
+});
+
 app.get("/hello" , function(req, res) {
   console.log("Nachricht kam an!");
   currentClientsws[1].send("255,161,20,100");
--- public/dashboard/dash.js
+++ public/dashboard/dash.js
@@ -2,3 +2,7 @@ function getLog(){
     window.open( 
         "http://zimmermatic:3443/DownloadLog", "_blank");
 }
+function getLogComplete(){
+    window.open( 
+        "http://zimmermatic:3443/DownloadLogCom", "_blank");
+}
