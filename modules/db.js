9dca0dfb74bccd54cb476d877e8a13b49f901296
MarvinHofmann <82021987+MarvinHofmann@users.noreply.github.com>
2021-09-19 14:45:53 +0200

    ++

--- modules/db.js
+++ modules/db.js
@@ -1,34 +1,45 @@
 const main = require("../index");
 
-const datenbank = require("nedb")
-const db = new datenbank({filename: '../Datenbanken/tempDB.db'});
+const datenbank = require("nedb");
+const db = new datenbank({ filename: "../Datenbanken/tempDB.db" });
 db.loadDatabase();
 
 function store(json) {
-    db.insert(json);
+  db.insert(json);
 }
 exports.store = store;
 
 function getAll() {
-    db.find({}, function (err, docs) {
-        console.log(docs);
-        return docs;
-    });
+  db.find({}, function (err, docs) {
+    console.log(docs);
+    return docs;
+  });
 }
 exports.getAll = getAll;
 
 exports.getTagesHoch = function () {
-    let a = new Date();
-    let tH = 0;
-    db.find({date: String(a.getDate()) + String(a.getMonth()+1) + String(a.getUTCFullYear())}).sort({temperatur: -1}).limit(1).exec(function (err,docs) {
-        console.log(docs[0].temperatur);   
-        tH = docs[0].temperatur;      
+  let a = new Date();
+  let tH = 0;
+  db.find({
+    date:
+      String(a.getDate()) +
+      String(a.getMonth() + 1) +
+      String(a.getUTCFullYear()),
+  })
+    .sort({ temperatur: -1 })
+    .limit(1)
+    .exec(function (err, docs) {
+      try {
+        console.log(docs[0].temperatur);
+        tH = docs[0].temperatur;
         for (let i = 0; i < main.ClientswsBrowser.length; i++) {
-            main.ClientswsBrowser[i].send(
-                JSON.stringify({ type: "High", value: tH })
-              );
+          main.ClientswsBrowser[i].send(
+            JSON.stringify({ type: "High", value: tH })
+          );
         }
-        return(String(tH));
-    });   
-}
-    
+        return String(tH);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+};
