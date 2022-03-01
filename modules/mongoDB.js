2982b65261636edd74ab4c6e7d7069a185ad3950
MarvinHofmann <82021987+MarvinHofmann@users.noreply.github.com>
2022-03-01 22:12:59 +0100

    Update mongoDB.js

--- modules/mongoDB.js
+++ modules/mongoDB.js
@@ -7,36 +7,42 @@ const uri = process.env.DB_URL;
 console.log(uri);
 
 const DBClient = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
 MongoClient.connect(uri)
-  .then(client => {
-    try {
-      const db = client.db('tempSensor');
-      const tempCollection = db.collection('values');
-      main.app.locals.collection = tempCollection;
-      console.log("connection erfolgt");
-    } catch (error) {
-      console.log(error);
-    }
-});
+    .then(client => {
+        try {
+            const db = client.db('tempSensor');
+            const tempCollection = db.collection('values');
+            main.app.locals.collection = tempCollection;
+            console.log("connection erfolgt");
+        } catch (error) {
+            console.log(error);
+        }
+    });
 
 function store(valObj) {
-    main.app.locals.collection.insertOne(valObj, function(err, res) {
+    main.app.locals.collection.insertOne(valObj, function (err, res) {
         if (err) throw err;
         console.log("1 document inserted");
     });
-
     const collection = main.app.locals.collection;
     collection.find({}).toArray()
-    .then(response => console.log(response));
+        .then(response => console.log(response));
 }
 exports.store = store;
 
-main.app.get("/store", (req,res) =>{
-    let obj = {sensor: "bett", temp: 22, hum: 46};
-    store(obj);
+main.app.get("/db/tempValues", (req, res) => {
+    const collection = main.app.locals.collection;
+    collection.distinct("temperatur")
+        .then(response => res.status(200).json(response))
+        .catch(error => console.error(error));
+    res.sendStatus(200);
+});
+
+main.app.get("/db/tageshoch", (req, res) => {
+    
     res.sendStatus(200);
 });
