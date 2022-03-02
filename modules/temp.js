283a4acc3a4fad4993bdad94da6be58028b45a5f
MarvinHofmann <82021987+MarvinHofmann@users.noreply.github.com>
2022-03-02 22:28:24 +0100

    jeden 6 wert storen => jeder 2. gesamtzyklus (30) min

--- modules/temp.js
+++ modules/temp.js
@@ -14,89 +14,102 @@ let temp,
   zeit1,
   zeit2,
   zeit3,
-  average;
+  average,
+  cntAverage;
 exports.temp = temp;
 exports.average = average;
 function broadcast(feucht, temp, zeit, sender) {
-    for (let i = 0; i < main.ClientswsBrowser.length; i++) {
-      main.ClientswsBrowser[i].send(
-        JSON.stringify({ type: "feuchtigkeit" + sender, value: feucht })
-      );
-      main.ClientswsBrowser[i].send(
-        JSON.stringify({ type: "temperatur" + sender, value: temp })
-      );
-      main.ClientswsBrowser[i].send(
-        JSON.stringify({ type: "zeit" + sender, value: zeit })
-      );
-      main.ClientswsBrowser[i].send(
-        JSON.stringify({ type: "average", value: average })
-      );
-    }
+  for (let i = 0; i < main.ClientswsBrowser.length; i++) {
+    main.ClientswsBrowser[i].send(
+      JSON.stringify({ type: "feuchtigkeit" + sender, value: feucht })
+    );
+    main.ClientswsBrowser[i].send(
+      JSON.stringify({ type: "temperatur" + sender, value: temp })
+    );
+    main.ClientswsBrowser[i].send(
+      JSON.stringify({ type: "zeit" + sender, value: zeit })
+    );
+    main.ClientswsBrowser[i].send(
+      JSON.stringify({ type: "average", value: average })
+    );
   }
+}
 
 /********************************Temperatursensoren******************************************* */
-let cntA = [0,0,0];
+let cntA = [0, 0, 0];
 main.app.post("/", function (req, res) {
   console.log("Angekommen: " + req.body.temperatur + " : " + req.body.feuchtigkeit + " S1");
-    temp = req.body.temperatur;
-    feucht = req.body.feuchtigkeit;
-    zeit1 = zeit.berechneZeit();
-    getTempAverage();
-    broadcast(feucht, temp, zeit1, "S1");
-    handleDB(0,feucht,temp);
-    res.sendStatus(200);
-  });
-  
-  main.app.post("/senderZwei", function (req, res) {
-    console.log("Angekommen: " + req.body.temperatur + " : " + req.body.feuchtigkeit + " S2");
-    temp2 = req.body.temperatur;
-    feucht2 = req.body.feuchtigkeit;
-    zeit2 = zeit.berechneZeit();
-    getTempAverage();
-    broadcast(feucht2, temp2, zeit2, "S2");
-    handleDB(1,feucht2,temp2);
-    res.sendStatus(200);
-  });
-  
-  main.app.post("/senderDrei", function (req, res) {
-    console.log("Angekommen: " + req.body.temperatur + " : " + req.body.feuchtigkeit + " S3");
-    temp3 = req.body.temperatur;
-    feucht3 = req.body.feuchtigkeit;
-    zeit3 = zeit.berechneZeit();
-    getTempAverage();
-    broadcast(feucht3, temp3, zeit3, "S3");
-    handleDB(2,feucht2,temp3);
-    res.sendStatus(200);
-  });
+  temp = req.body.temperatur;
+  feucht = req.body.feuchtigkeit;
+  zeit1 = zeit.berechneZeit();
+  getTempAverage();
+  broadcast(feucht, temp, zeit1, "S1");
+  handleDB(0, feucht, temp);
+  res.sendStatus(200);
+});
+
+main.app.post("/senderZwei", function (req, res) {
+  console.log("Angekommen: " + req.body.temperatur + " : " + req.body.feuchtigkeit + " S2");
+  temp2 = req.body.temperatur;
+  feucht2 = req.body.feuchtigkeit;
+  zeit2 = zeit.berechneZeit();
+  getTempAverage();
+  broadcast(feucht2, temp2, zeit2, "S2");
+  handleDB(1, feucht2, temp2);
+  res.sendStatus(200);
+});
+
+main.app.post("/senderDrei", function (req, res) {
+  console.log("Angekommen: " + req.body.temperatur + " : " + req.body.feuchtigkeit + " S3");
+  temp3 = req.body.temperatur;
+  feucht3 = req.body.feuchtigkeit;
+  zeit3 = zeit.berechneZeit();
+  getTempAverage();
+  broadcast(feucht3, temp3, zeit3, "S3");
+  handleDB(2, feucht2, temp3);
+  res.sendStatus(200);
+});
 
 
 //schließe Rolladen, wenn wärmer als 24 ° Durchschnitt
 function getTempAverage() {
-  let a = new Date();
+  if (cntAverage == 6) {
+    cntAverage = 0;
+    let a = new Date();
     averageHum = ((feucht + feucht2 + feucht3) / 3).toFixed(2);
     average = ((temp + temp2 + temp3) / 3).toFixed(2);
     let obj = {
       feuchtigkeit: averageHum,
       temperatur: average,
       date:
-      String(a.getDate()) + ":" +
-      String(a.getMonth() + 1) + ":" +
-      String(a.getUTCFullYear()),
+        String(a.getDate()) + ":" +
+        String(a.getMonth() + 1) + ":" +
+        String(a.getUTCFullYear()),
       time: String(a.getHours()) + ":" + String(a.getMinutes()),
       timestamp: new Date()
     }
     db.storeMedian(obj, function (err) {
       if (err) {
         console.log(err);
-      }else{
+      } else {
         console.log("ferfolgreich geschrieben");
       }
     })
-    if (average > 24 /*&& main.status === true*/) {
-      //tel.sendM("Fahre Rolladen runter Temperatur >24°");
-      rol.rolladenDown();
-     // main.status = false; //setze status
+  }
+  averageHum = ((feucht + feucht2 + feucht3) / 3).toFixed(2);
+  average = ((temp + temp2 + temp3) / 3).toFixed(2);
+  db.storeMedian(obj, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("ferfolgreich geschrieben");
     }
+  })
+  if (average > 24 /*&& main.status === true*/) {
+    //tel.sendM("Fahre Rolladen runter Temperatur >24°");
+    rol.rolladenDown();
+    // main.status = false; //setze status
+  }
 
 }
 exports.getTempAverage = getTempAverage;
@@ -105,19 +118,19 @@ function getFeuchtAverage() {
   return ((feucht + feucht2 + feucht3) / 3).toFixed(2);
 }
 
-function publish(){
-    broadcast(feucht, temp, zeit1, "S1");
-    broadcast(feucht2, temp2, zeit2, "S2");
-    broadcast(feucht3, temp3, zeit3, "S3");
+function publish() {
+  broadcast(feucht, temp, zeit1, "S1");
+  broadcast(feucht2, temp2, zeit2, "S2");
+  broadcast(feucht3, temp3, zeit3, "S3");
 }
 exports.publish = publish;
 
-exports.botSendStatus = function(){
-  tel.sendM( 
-    "Durchschn. Temp: " + average 
-  + "Temp1: " + temp
-  + "Temp2: " + temp2
-  + "Temp3: " + temp3);
+exports.botSendStatus = function () {
+  tel.sendM(
+    "Durchschn. Temp: " + average
+    + "Temp1: " + temp
+    + "Temp2: " + temp2
+    + "Temp3: " + temp3);
 }
 
 function handleDB(sender, feuchtIn, tempIn) {
@@ -129,9 +142,9 @@ function handleDB(sender, feuchtIn, tempIn) {
       feuchtigkeit: feuchtIn,
       temperatur: tempIn,
       date:
-      String(a.getDate()) + ":" +
-      String(a.getMonth() + 1) + ":" +
-      String(a.getUTCFullYear()),
+        String(a.getDate()) + ":" +
+        String(a.getMonth() + 1) + ":" +
+        String(a.getUTCFullYear()),
       time: String(a.getHours()) + ":" + String(a.getMinutes()),
       timestamp: new Date()
     };
@@ -139,7 +152,7 @@ function handleDB(sender, feuchtIn, tempIn) {
     db.store(jsonT, function (err) {
       if (err != null) {
         console.log(err);
-      }else{
+      } else {
         console.log("erfolgreich gestored");
       }
     });
@@ -147,13 +160,13 @@ function handleDB(sender, feuchtIn, tempIn) {
   }
 }
 
-exports.publishDash = function(){
+exports.publishDash = function () {
   for (let i = 0; i < main.ClientswsBrowser.length; i++) {
     main.ClientswsBrowser[i].send(
       JSON.stringify({ type: "Temp", value: average })
     );
     main.ClientswsBrowser[i].send(
-      JSON.stringify({ type: "Feucht" , value: getFeuchtAverage() })
+      JSON.stringify({ type: "Feucht", value: getFeuchtAverage() })
     );
   }
 }
